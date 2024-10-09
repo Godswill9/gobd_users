b6b59c1d2786fa0f873a4dc7397d319ef96d07af
unknown <ghycinth9@gmail.com>
2024-10-10 00:55:02 +0100

    more

--- src/components/testPage.jsx
+++ src/components/testPage.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
 import "../../stylings/styles.css"
+import React, { useState, useEffect } from 'react';
 
-export default function TestPage() {
+function TestPage() {
+  const [carDetails, setCarDetails] = useState({
+    carMake: '',
+    carBrand: '',
+    carYear: '',
+    carEngineType: '',
+  });
+
+  useEffect(() => {
+    // Read car details from cookies
+    const carMake = readCookie('carMake');
+    const carBrand = readCookie('carBrand');
+    const carYear = readCookie('carYear');
+    const carEngineType = readCookie('carEngineType');
+    const faultCode = readCookie('faultCode');
+
+    // Set car details in state
+    setCarDetails({
+      carMake,
+      carBrand,
+      carYear,
+      carEngineType,
+      faultCode
+    });
+  }, []);
+
   return (
-    <div>
-        <div className="headTest">Hello, this is the test page</div>
+    <div className="headTest">
+      <div>Hello, this is the test page</div>
+      <p>Car Make: {carDetails.carMake}</p>
+      <p>Car Brand: {carDetails.carBrand}</p>
+      <p>Car Year: {carDetails.carYear}</p>
+      <p>Car Engine Type: {carDetails.carEngineType}</p>
+      <p>Car faultCode: {carDetails.faultCode}</p>
     </div>
-  )
+  );
 }
+
+export default TestPage;
