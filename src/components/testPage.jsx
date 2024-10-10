fcc926c036a9d55e802150397bc285a8b8193893
unknown <ghycinth9@gmail.com>
2024-10-10 07:27:49 +0100

    more chages

--- src/components/testPage.jsx
+++ src/components/testPage.jsx
@@ -1,41 +1,34 @@
 import "../../stylings/styles.css";
 import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
 import { useParams } from "react-router-dom";
 
 function TestPage() {
-  const [carDetails, setCarDetails] = useState({
-    carMake: '',
-    carBrand: '',
-    carYear: '',
-    carEngineType: '',
-    faultCode: '',
-  });
-  const {car} = useParams();
+  const { car } = useParams();
   
+  // Split the car string to get individual parameters
+  const params = new URLSearchParams(car.replace(/&/g, '&'));
+  const carMake = params.get('a') || '';
+  const carBrand = params.get('b') || '';
+  const carYear = params.get('c') || '';
+  const carEngineType = params.get('d') || '';
+  const faultCode = params.get('e') || '';
 
-  useEffect(() => {
-    // Read car details from cookies
-    const carMake = Cookies.get('carMake') || '';
-    const carBrand = Cookies.get('carBrand') || '';
-    const carYear = Cookies.get('carYear') || '';
-    const carEngineType = Cookies.get('carEngineType') || '';
-    const faultCode = Cookies.get('faultCode') || '';
-
-    // Set car details in state
-    setCarDetails({
-      carMake,
-      carBrand,
-      carYear,
-      carEngineType,
-      faultCode,
-    });
-  }, []);
+  const [carDetails, setCarDetails] = useState({
+    carMake,
+    carBrand,
+    carYear,
+    carEngineType,
+    faultCode,
+  });
 
   return (
     <div className="headTest">
       <div>Hello, this is the test page</div>
-      <p>Car Make: {car}</p>
+      <p>Car Make: {carDetails.carMake}</p>
+      <p>Car Brand: {carDetails.carBrand}</p>
+      <p>Car Year: {carDetails.carYear}</p>
+      <p>Car Engine Type: {carDetails.carEngineType}</p>
+      <p>Fault Code: {carDetails.faultCode}</p>
     </div>
   );
 }
